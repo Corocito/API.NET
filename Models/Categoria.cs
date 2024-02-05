cc2d6e6fd194e7e7ac3d4f598d6c12127cfc7ec3
Corozco <orozcomondragons@gmail.com>
2024-02-05 15:27:26 -0500

    se realizan pequeñas actualizaciones con los datos de los modelos

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -6,10 +6,10 @@ namespace webapi.Models;
 public class Categoria
 {
     public Guid CategoriaId {get;set;}
-    public string Nombre {get;set;}
-    public string Descripcion {get;set;}
+    public string? Nombre {get;set;}
+    public string? Descripcion {get;set;}
     public int Peso {get;set;}
 
     [JsonIgnore]
-    public virtual ICollection<Tarea> Tareas {get;set;}
+    public virtual ICollection<Tarea>? Tareas {get;set;}
 }
